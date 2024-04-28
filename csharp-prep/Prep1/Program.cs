6b2e143c34ad0c4eeedcfc899ff70569f7639e8f
Laerry Mahonri <laerry1258@gmail.com>
2024-04-28 15:20:43 -0600

    asking name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Hello {last}, your full name is  {first} {last}.");
     }
 }
